9b35783b0d4eab24adee8cad50c074e95ad257d7
Rodrigo15A <rodri150306@gmail.com>
2021-10-25 18:08:38 -0500

    segunda actualizacion rp

---
+++ Numero mayor.php
@@ -0,0 +1,17 @@
+<?
+$numero1= 10;
+$numero2= 15;
+$numero3= 20;
+
+
+if ($numero1>$numero2 and $numero1>$numero3){
+echo "el numero mayor es: " .$numero1;
+}
+
+elseif ($numero2>$numero3);{
+    echo "el numero mayor es: " .$numero2;
+}
+else (){
+    echo"el numero mayor es: " .$numero3; }
+    
+ ?>
---
+++ interfaz.php
@@ -0,0 +1,11 @@
+<?
+$nombre= "ana";
+$contraseña= "123";
+switch ($nombre){
+    case "ana":
+        echo "bienvenida".$nombre . "al programa";
+        break;
+    case "carlos";
+        echo "bienvenido".$nombre . "al programa";
+        
+}
